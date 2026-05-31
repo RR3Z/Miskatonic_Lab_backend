cc93e4a2f441ba7315188ee2f1e458d4a1fb212d
Chernobaev Egor <consicration@mail.ru>
2026-05-31 19:45:48 +0300

    add: server run and shutdown logic

---
+++ server.go
@@ -0,0 +1,29 @@
+package MiskatonicLab
+
+import (
+	"context"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func (s *Server) Run(port string, handler http.Handler) error {
+	address := ":" + port
+
+	s.httpServer = &http.Server{
+		Addr:           address,
+		Handler:        handler,
+		MaxHeaderBytes: 1024 * 1024, // 1MB
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+	}
+
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
