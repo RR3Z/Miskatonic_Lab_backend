562852228767069ae7980d4003432e7344c8aba9
Chernobaev Egor <consicration@mail.ru>
2026-06-01 20:22:26 +0300

    update: connect CORS to handler.go and main.go

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 
 	MiskatonicLab "github.com/RR3Z/Miskatonic_Lab_backend"
 	"github.com/RR3Z/Miskatonic_Lab_backend/pkg/handler"
+	"github.com/RR3Z/Miskatonic_Lab_backend/pkg/utils"
 	"github.com/clerk/clerk-sdk-go/v2"
 	"github.com/joho/godotenv"
 )
@@ -23,8 +24,16 @@ func main() {
 	}
 	clerk.SetKey(clerkSecretKey)
 
+	// Allowed Origins for CORS in Handler
+	allowedOrigins := utils.ParseAllowedOrigins(os.Getenv("CORS_ALLOWED_ORIGINS"))
+	if len(allowedOrigins) == 0 {
+		log.Fatal("[CORS] ERROR because CORS_ALLOWED_ORIGINS is not set")
+	}
+
 	// Server
-	handlers := new(handler.Handler)
+	handlers := &handler.Handler{
+		AllowedOrigins: allowedOrigins,
+	}
 	server := new(MiskatonicLab.Server)
 	if err := server.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("[HTTP_SERVER] ERROR while running http server: %v", err)
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -3,13 +3,23 @@ package handler
 import (
 	"github.com/RR3Z/Miskatonic_Lab_backend/pkg/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 )
 
 type Handler struct {
+	AllowedOrigins []string
 }
 
 func (h *Handler) InitRoutes() *chi.Mux {
 	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   h.AllowedOrigins,
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 
 	router.Route("/health", func(r chi.Router) {
 		r.Get("/", h.health)
