c856879e51d79f7d95788729725b17438bd20468
Chernobaev Egor <consicration@mail.ru>
2026-06-01 23:41:27 +0300

    update: connect CORSMiddleware to handler

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -3,23 +3,22 @@ package handler
 import (
 	"github.com/RR3Z/Miskatonic_Lab_backend/pkg/middleware"
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/cors"
 )
 
 type Handler struct {
-	AllowedOrigins []string
+	corsConfig middleware.CORSConfig
+}
+
+func NewHandler(corsConfig middleware.CORSConfig) *Handler {
+	return &Handler{
+		corsConfig: corsConfig,
+	}
 }
 
 func (h *Handler) InitRoutes() *chi.Mux {
 	router := chi.NewRouter()
 
-	router.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   h.AllowedOrigins,
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
-		AllowCredentials: false,
-		MaxAge:           300,
-	}))
+	router.Use(middleware.CORSMiddleware(h.corsConfig))
 
 	router.Route("/health", func(r chi.Router) {
 		r.Get("/", h.health)
