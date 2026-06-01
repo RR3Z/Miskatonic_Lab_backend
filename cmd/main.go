ed06a42edb1ed1d62bc3ebc3d47cc22950645657
Chernobaev Egor <consicration@mail.ru>
2026-06-01 20:06:13 +0300

    refactor: main.go comments and logs

--- cmd/main.go
+++ cmd/main.go
@@ -11,18 +11,20 @@ import (
 )
 
 func main() {
+	// Load ENVs
 	if err := godotenv.Load(); err != nil {
-		log.Println("[ENV] .env file was not loaded, using system environment variables")
+		log.Println("[ENV] ERROR .env file was not loaded, using system environment variables")
 	}
 
+	// Connect Clerk SDK
 	clerkSecretKey := os.Getenv("CLERK_SECRET_KEY")
 	if clerkSecretKey == "" {
-		log.Fatal("[CLERK] CLERK_SECRET_KEY is not set")
+		log.Fatal("[CLERK] ERROR because CLERK_SECRET_KEY is not set")
 	}
 	clerk.SetKey(clerkSecretKey)
 
+	// Server
 	handlers := new(handler.Handler)
-
 	server := new(MiskatonicLab.Server)
 	if err := server.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("[HTTP_SERVER] ERROR while running http server: %v", err)
