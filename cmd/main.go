a3feaefb6ffd392723438ffddf9134353972a234
Chernobaev Egor <consicration@mail.ru>
2026-06-01 23:43:16 +0300

    update: prepare corsConfig in main.go now

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 	MiskatonicLab "github.com/RR3Z/Miskatonic_Lab_backend"
 	"github.com/RR3Z/Miskatonic_Lab_backend/pkg/config"
 	"github.com/RR3Z/Miskatonic_Lab_backend/pkg/handler"
+	"github.com/RR3Z/Miskatonic_Lab_backend/pkg/middleware"
 	"github.com/clerk/clerk-sdk-go/v2"
 	"github.com/joho/godotenv"
 )
@@ -24,23 +25,25 @@ func main() {
 	}
 	clerk.SetKey(clerkSecretKey)
 
-	// Allowed Origins for CORS in Handler
+	// Configure CORS
 	allowedOrigins := config.ParseAllowedOrigins(os.Getenv("CORS_ALLOWED_ORIGINS"))
 	if len(allowedOrigins) == 0 {
-		log.Fatal("[CORS] ERROR because CORS_ALLOWED_ORIGINS is not set")
+		log.Fatalf("[main -> CORS] ERROR because CORS_ALLOWED_ORIGINS is not set in .env")
 	}
-
-	// Server
-	handlers := &handler.Handler{
+	corsConfig := middleware.CORSConfig{
 		AllowedOrigins: allowedOrigins,
 	}
-	server := new(MiskatonicLab.Server)
+
+	// Launch Server
+	handlers := handler.NewHandler(corsConfig)
 
 	serverPort := os.Getenv("PORT")
 	if serverPort == "" {
 		log.Fatal("[HTTP_SERVER] ERROR because PORT is not set (the default port will be used - 8000)")
 		serverPort = "8000"
 	}
+
+	server := new(MiskatonicLab.Server)
 
 	if err := server.Run(serverPort, handlers.InitRoutes()); err != nil {
 		log.Fatalf("[HTTP_SERVER] ERROR while running http server: %v", err)
