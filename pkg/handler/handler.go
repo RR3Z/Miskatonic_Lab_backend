c7e089dee92152b1ae8679c955aea1040af2d3c8
Chernobaev Egor <consicration@mail.ru>
2026-06-02 20:55:37 +0300

    update: user data route from GET /userData to GET /me

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -28,9 +28,7 @@ func (h *Handler) InitRoutes() *chi.Mux {
 	router.Route("/api", func(r chi.Router) {
 		r.Use(middleware.AuthMiddleware)
 
-		r.Route("/userData", func(r chi.Router) {
-			r.Get("/", h.getUserByClerkID)
-		})
+		r.Get("/me", h.getUserByClerkID)
 
 		r.Route("/characters", func(r chi.Router) {
 			r.Post("/", h.createCharacter)
