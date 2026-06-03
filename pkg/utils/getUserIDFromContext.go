b23b2772ea9cff1df21090a6ec97116e36a471ed
Chernobaev Egor <consicration@mail.ru>
2026-06-03 14:10:48 +0300

    update: removed checks from GetUserIDFromContext as it is used with AuthMiddleware

--- pkg/utils/getUserIDFromContext.go
+++ pkg/utils/getUserIDFromContext.go
@@ -2,31 +2,11 @@ package utils
 
 import (
 	"context"
-	"errors"
-	"log/slog"
-	"strings"
 
 	"github.com/clerk/clerk-sdk-go/v2"
 )
 
-func GetUserIDFromContext(ctx context.Context) (string, error) {
-	claims, ok := clerk.SessionClaimsFromContext(ctx)
-	if !ok {
-		slog.Error(
-			"failed to get clerk session claims",
-			"component", "character_api",
-		)
-		return "", errors.New("unauthorized")
-	}
-
-	userID := claims.Subject
-	if strings.TrimSpace(userID) == "" {
-		slog.Error(
-			"clerk session claims missing subject",
-			"component", "user_api",
-		)
-		return "", errors.New("unauthorized")
-	}
-
-	return userID, nil
+func GetUserIDFromContext(ctx context.Context) string {
+	claims, _ := clerk.SessionClaimsFromContext(ctx)
+	return claims.Subject
 }
