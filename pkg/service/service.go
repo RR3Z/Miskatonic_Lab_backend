a5924168f952ab1ed80a5814b2efbbc5d9eb3acf
Chernobaev Egor <consicration@mail.ru>
2026-06-10 14:25:20 +0300

    update: connect DiceRoller service to Service

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -3,19 +3,22 @@ package service
 import (
 	"github.com/RR3Z/Miskatonic_Lab_backend/pkg/events"
 	"github.com/RR3Z/Miskatonic_Lab_backend/pkg/repository"
-	characterServices "github.com/RR3Z/Miskatonic_Lab_backend/pkg/service/character"
+	"github.com/RR3Z/Miskatonic_Lab_backend/pkg/service/character"
+	"github.com/RR3Z/Miskatonic_Lab_backend/pkg/service/diceRoller"
 )
 
 type Service struct {
-	User      IUser
-	Character characterServices.ICharacter
+	User       IUser
+	Character  character.ICharacter
+	DiceRoller diceRoller.IDiceRoller
 }
 
 func NewService(repos *repository.Repository, publisher events.EventPublisher) *Service {
-	characterService := characterServices.NewCharacterService(repos, publisher)
+	characterService := character.NewCharacterService(repos, publisher)
 
 	return &Service{
-		User:      NewUserService(repos),
-		Character: characterServices.NewEventPublishingCharacterService(characterService, publisher),
+		User:       NewUserService(repos),
+		Character:  character.NewEventPublishingCharacterService(characterService, publisher),
+		DiceRoller: diceRoller.NewDiceRollerService(repos),
 	}
 }
